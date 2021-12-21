d02f2d62b5945784b14afd9ccc74a12b3c32da11
angad737 <angad737@gmail.com>
2021-12-21 14:14:58 +0530

    Snapshot - Created Course Home Page basic fcde

--- pages/study/index.js
+++ pages/study/index.js
@@ -1,8 +1,22 @@
+import Link from "next/link";
+
 const StudyHomePage = (props) => {
   return (
     <div>
       <h1>Study</h1>
       <p>This page is under development...</p>
+      <ul>
+        <li>
+          <Link href="/study/a320">
+            <a>A320</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/study/navigation">
+            <a>General Navigation</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
---
+++ pages/study/navigation/index.js
@@ -0,0 +1,10 @@
+const CourseHomePage = (props) => {
+  return (
+    <div>
+      <h1>Course Home Page</h1>
+      <p>This is course Home Page for COURSE TITLE HERE.</p>
+    </div>
+  );
+};
+
+export default CourseHomePage;
